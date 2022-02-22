95999f029c4efeb9ef088eaedde495786567f1a6
Paul Wilson <pfrwilson@gmail.com>
2022-02-22 16:48:51 -0500

    add main training loop

--- main.py
+++ main.py
@@ -6,4 +6,38 @@ from omegaconf import DictConfig
 @hydra.main(config_path='config', config_name='config')
 def main(config: DictConfig):
 
-    train_dataset = M
+    from src.data import DataModule
+    dm = DataModule(config.data)
+
+    module = None
+    assert config.mode in ['coteaching', 'vanilla']
+    if config.mode == 'coteaching':
+
+        from src.coteaching import CoteachingModule
+        module = CoteachingModule(
+            config.model,
+            config.loss,
+            config.opt_config,
+        )
+
+    elif config.mode == 'vanilla':
+
+        from src.coteaching import VanillaModule
+        module = VanillaModule(
+            config.model,
+            config.loss,
+            config.opt_config
+        )
+
+    from src.logging import get_logger
+    logger = get_logger(config.logger)
+
+    from pytorch_lightning import Trainer
+    trainer = Trainer(**config.trainer, logger=logger)
+
+    trainer.fit(module, dm)
+
+
+if __name__ == '__main__':
+    main()
+
