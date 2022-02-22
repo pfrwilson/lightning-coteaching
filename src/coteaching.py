e290a85e5a970473e5a524aae713f2dc10c6ee7c
Paul Wilson <pfrwilson@gmail.com>
2022-02-22 13:25:12 -0500

    add update .gitignore

--- src/coteaching.py
+++ src/coteaching.py
@@ -15,12 +15,11 @@ def get_loss(loss_config):
         )
 
 
-class CoteachingMNIST(LightningModule):
+class CoteachingModule(LightningModule):
     
     def __init__(self, model_config: DictConfig,
                  loss_config: DictConfig,
-                 final_forget_rate: float,
-                 final_forget_rate_epoch: int):
+                 opt_config: DictConfig):
 
         super().__init__()
 
@@ -29,26 +28,25 @@ class CoteachingMNIST(LightningModule):
 
         self.loss_fn = get_loss(loss_config)
 
-        self.forget_rate = final_forget_rate
-        self.final_forget_rate_epoch = final_forget_rate_epoch
+        self.opt_config = opt_config
+
+        self.train_accuracy = Accuracy(num_classes=10)
+        self.val_accuracy = Accuracy(num_classes=10)
 
         self.automatic_optimization = False
-
-        self.accuracy = Accuracy(num_classes=10)
 
         self.save_hyperparameters()
 
     def configure_optimizers(self):
 
-        opt1 = torch.optim.Adam(self.model1.parameters())
-        opt2 = torch.optim.Adam(self.model2.parameters())
+        opt1 = torch.optim.Adam(self.model1.parameters(), lr=self.opt_config.lr)
+        opt2 = torch.optim.Adam(self.model2.parameters(), lr=self.opt_config.lr)
 
-        return opt1, opt2
+        return [opt1, opt2]
 
     def training_step(self, batch, batch_idx):
-        
         x, y = batch
-        
+
         logits_1 = self.model1(x)
         logits_2 = self.model2(x)
 
@@ -57,11 +55,13 @@ class CoteachingMNIST(LightningModule):
         opt1, opt2 = self.optimizers()
 
         opt1.zero_grad()
+        opt2.zero_grad()
         self.manual_backward(loss_1)
         opt2.step()
 
+        opt1.zero_grad()
         opt2.zero_grad()
-        self.manual_backward(loss_2)
+        self.manual_backward(loss_2, retain_graph=True)
         opt2.step()
 
         self.log_dict({'loss_1': loss_1, 'loss_2': loss_2})
@@ -72,21 +72,23 @@ class CoteachingMNIST(LightningModule):
 
         logits_1 = self.model1(x)
 
-        self.accuracy(logits_1, y)
+        self.val_accuracy(logits_1, y)
 
-        self.log('val/accuracy', self.accuracy, on_step=False, on_epoch=True)
+        self.log('val/accuracy', self.val_accuracy, on_step=False, on_epoch=True)
 
     def coteaching_loss(self, logits_1, logits_2, targets):
 
-        batch_size = logits_1.size[0]
+        batch_size = logits_1.shape[0]
 
         num_to_keep = int((1 - self.get_forget_rate()) * batch_size)
 
         loss_1 = self.loss_fn(logits_1, targets)
         loss_2 = self.loss_fn(logits_2, targets)
 
-        loss_1_clean_indices = torch.sort(loss_2).indices[:num_to_keep]
-        loss_2_clean_indices = torch.sort(loss_1).indices[:num_to_keep]
+        ind_1_sorted = torch.argsort(loss_1.data.cpu())
+        ind_2_sorted = torch.argsort(loss_2.data.cpu())
+        loss_1_clean_indices = ind_1_sorted[:num_to_keep]
+        loss_2_clean_indices = ind_2_sorted[:num_to_keep]
 
         loss_1_clean = torch.mean(self.loss_fn(
             logits_1[loss_1_clean_indices],
@@ -103,8 +105,51 @@ class CoteachingMNIST(LightningModule):
     def get_forget_rate(self):
 
         t = self.current_epoch
-        t_k = self.final_forget_rate_epoch
+        t_k = self.opt_config.final_forget_rate_epoch
 
         ratio = min(t / t_k, 1)
-        return self.final_forget_rate * ratio
+        return self.opt_config.final_forget_rate * ratio
 
+
+class VanillaModule(LightningModule):
+
+    def __init__(self, model_config, loss_config, opt_config):
+        super().__init__()
+
+        self.model = build_model(model_config)
+
+        self.loss_fn = get_loss(loss_config)
+
+        self.opt_config = opt_config
+
+        self.train_accuracy = Accuracy(num_classes=10)
+        self.val_accuracy = Accuracy(num_classes=10)
+
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+
+        return torch.optim.Adam(self.model.parameters(), self.opt_config.lr)
+
+    def training_step(self, batch, batch_idx):
+
+        x, y = batch
+
+        logits = self.model(x)
+
+        loss = torch.mean(self.loss_fn(logits, y))
+        self.train_accuracy(logits, y)
+
+        self.log('train/loss', loss, on_step=True)
+        self.log('train/accuracy', self.train_accuracy, on_step=False, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+
+        x, y = batch
+        logits = self.model(x)
+
+        self.val_accuracy(logits, y)
+
+        self.log('val/accuracy', self.val_accuracy, on_step=False, on_epoch=True)
