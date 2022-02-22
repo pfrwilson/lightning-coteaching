468fc868ec155c07a88aea4c05cdef9826160fec
Paul Wilson <pfrwilson@gmail.com>
2022-02-22 16:56:12 -0500

    add model factory

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,22 @@
 
+from torch import nn 
+from .classifier import CNN
+
+from dataclasses import dataclass
+
 from omegaconf import DictConfig
 
-
+    
 def build_model(config: DictConfig):
-    from .classifier import CNN
-
-    return CNN(**config)
+    
+    if config.type == 'logistic_regression':
+        
+        return nn.Sequential(
+            nn.Flatten(), 
+            nn.Linear(28 * 28, 10)
+        )
+        
+    elif config.type == 'cnn':
+        
+        return CNN()
+    
