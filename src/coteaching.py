580c9cef7c8be421eee73f8ad030e1f80817b686
Paul Wilson <pfrwilson@gmail.com>
2022-02-24 19:53:51 -0500

    bug fixes; added training loop

--- src/coteaching.py
+++ src/coteaching.py
@@ -2,6 +2,8 @@
 from pytorch_lightning import LightningModule
 from omegaconf import DictConfig
 import torch
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 from torchmetrics import Accuracy
 
 from .models import build_model
@@ -32,10 +34,12 @@ class CoteachingModule(LightningModule):
 
         self.train_accuracy = Accuracy(num_classes=10)
         self.val_accuracy = Accuracy(num_classes=10)
-
-        #self.automatic_optimization = False
+        self.test_accuracy = Accuracy(num_classes=10)
 
         self.save_hyperparameters()
+
+    def forward(self, x):
+        return self.model1(x)
 
     def configure_optimizers(self):
 
@@ -45,35 +49,45 @@ class CoteachingModule(LightningModule):
         return [opt1, opt2]
 
     def training_step(self, batch, batch_idx, optimizer_idx):
-        x, y = batch
+        
+        x = batch[0]
+        y = batch[1]
+        noise_indices = batch[2]
 
         logits_1 = self.model1(x)
         logits_2 = self.model2(x)
 
+        self.train_accuracy(logits_1, y)
+
         losses = self.coteaching_loss(logits_1, logits_2, y)
 
-        #opt_1.zero_grad()
-        #self.manual_backward(loss_2, retain_graph=True)
-        #opt_2.zero_grad()
-        #self.manual_backward(loss_2)
-
-        #opt_1.step()
-        #opt_2.step()
-
+        self.log('train/accuracy', self.train_accuracy, on_step=False, on_epoch=True)
         self.log_dict({'train/loss_1': losses[0], 'train/loss_2': losses[1]})
 
         return losses[optimizer_idx]
 
     def validation_step(self, batch, batch_idx):
 
-        x, y = batch
+        x = batch[0]
+        y = batch[1]
 
         logits_1 = self.model1(x)
 
         self.val_accuracy(logits_1, y)
 
         self.log('val/accuracy', self.val_accuracy, on_step=False, on_epoch=True)
+        self.log('forget_rate', self.get_forget_rate(), on_step=False, on_epoch=True)
 
+    def test_step(self, batch, batch_idx):
+
+        x, y = batch
+
+        logits_1 = self.model1(x)
+
+        self.test_accuracy(logits_1, y)
+
+        self.log('test/accuracy', self.test_accuracy, on_step=False, on_epoch=True)
+                         
     def coteaching_loss(self, logits_1, logits_2, targets):
 
         batch_size = logits_1.shape[0]
@@ -85,20 +99,20 @@ class CoteachingModule(LightningModule):
 
         ind_1_sorted = torch.argsort(loss_1)
         ind_2_sorted = torch.argsort(loss_2)
-        loss_1_clean_indices = ind_1_sorted[:num_to_keep]
-        loss_2_clean_indices = ind_2_sorted[:num_to_keep]
+        ind_1_clean = ind_1_sorted[:num_to_keep]
+        ind_2_clean = ind_2_sorted[:num_to_keep]
 
         loss_1_clean = torch.mean(self.loss_fn(
-            logits_1[loss_1_clean_indices],
-            targets[loss_1_clean_indices]
+            logits_1[ind_1_clean],
+            targets[ind_1_clean]
         ))
 
         loss_2_clean = torch.mean(self.loss_fn(
-            logits_2[loss_2_clean_indices],
-            targets[loss_2_clean_indices]
+            logits_2[ind_2_clean],
+            targets[ind_2_clean]
         ))
 
-        return loss_1_clean, loss_2_clean
+        return loss_1_clean, loss_2_clean, ind_1_clean, ind_2_clean
 
     def get_forget_rate(self):
 
@@ -107,47 +121,3 @@ class CoteachingModule(LightningModule):
 
         ratio = min(t / t_k, 1)
         return self.opt_config.final_forget_rate * ratio
-
-
-class VanillaModule(LightningModule):
-
-    def __init__(self, model_config, loss_config, opt_config):
-        super().__init__()
-
-        self.model = build_model(model_config)
-
-        self.loss_fn = get_loss(loss_config)
-
-        self.opt_config = opt_config
-
-        self.train_accuracy = Accuracy(num_classes=10)
-        self.val_accuracy = Accuracy(num_classes=10)
-
-        self.save_hyperparameters()
-
-    def configure_optimizers(self):
-
-        return torch.optim.Adam(self.model.parameters(), self.opt_config.lr)
-
-    def training_step(self, batch, batch_idx):
-
-        x, y = batch
-
-        logits = self.model(x)
-
-        loss = torch.mean(self.loss_fn(logits, y))
-        self.train_accuracy(logits, y)
-
-        self.log('train/loss', loss, on_step=True)
-        self.log('train/accuracy', self.train_accuracy, on_step=False, on_epoch=True)
-
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-
-        x, y = batch
-        logits = self.model(x)
-
-        self.val_accuracy(logits, y)
-
-        self.log('val/accuracy', self.val_accuracy, on_step=False, on_epoch=True)
